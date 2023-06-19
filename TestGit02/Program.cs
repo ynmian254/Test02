077a8e01f2e43554794ca8d487644d6d1e50a76b
Yasser Naeem Mian <ynmian@yahoo.com>
2023-06-18 22:53:10 -0500

    changes 2

--- TestGit02/Program.cs
+++ TestGit02/Program.cs
@@ -7,7 +7,7 @@ namespace TestGit02
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Yasser");
         }
     }
 }
