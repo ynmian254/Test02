50544ff84d66993851bd929bd0bb163a243a2748
Yasser Naeem Mian <ynmian@yahoo.com>
2023-06-18 22:51:04 -0500

    test

---
+++ TestGit02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestGit02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
